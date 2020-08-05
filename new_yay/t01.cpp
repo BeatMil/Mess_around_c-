c25a8746692222b811fc2493194348181a4ea46f
beatmil <anutorn@gmail.com>
2020-08-05 12:04:17 +0700

    learning algorithm

---
+++ new_yay/t01.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /* I'm getting back to c++ and learn algorithm!
+     This code is just me messing around :) */
+    // and also this is one line comment
+    
+
+    int my_int;
+    my_int = 16;
+
+    cout << "Maika is the kawaiiest! >///<" << endl;
+    cout << "may be this way is better?\n";
+    cout << my_int << endl;
+    return 0;
+}
